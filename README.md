4284b8d798d8a38b11cfa9528530c487637150c7
Leoozito <leoanrdonogueira616@gmail.com>
2023-03-17 16:00:14 -0300

    feat(Adicionais no código): :sparkles: Model Serializers
    Adicionado serializers

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Sobre mais do que aprendi
 
 O Django em si é escrito de um monte de código python diferente e se souber como usa-lo corretamente,
-posso fazer as coisas mais rapido e mais facil
+posso fazer as coisas mais rapido e mais facil.
--- backend/api/views.py
+++ backend/api/views.py
@@ -4,18 +4,16 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.forms.models import model_to_dict
 from products.models import Product
+from products.serializers import ProductSerializer
 
 @api_view(["GET"]) # obtendo visu de API e no parenteses definindo os metodos de solicitação permitidos
 def api_home(request, *args, **kwargs):
     """
     DRF API VIEW
     """
-    
-    # if request.method != "POST":
-    #     return Response({"detail": "GET not allowed"}, status=405) # tratando erro http
-    model_data = Product.objects.all().order_by("?").first()
+    instance = Product.objects.all().order_by("?").first()
     data = {}
-    
-    if model_data:
-        data = model_to_dict(model_data, fields=['id', 'title', 'price'])
+    if instance:
+        data = ProductSerializer(instance).data
+        # data = model_to_dict(model_data, fields=['id', 'title', 'price', 'sale_price'])
     return Response(data)

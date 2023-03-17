576edce10a9e3c2115d7fe38f0d20ba687b3dacb
Leoozito <leoanrdonogueira616@gmail.com>
2023-03-17 14:37:19 -0300

    feat(Adicionais no código): :fire: View & Response
    REST framework instalado, imports novos, e visualização de API

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,19 +1,21 @@
 import json
-from django.http import JsonResponse, HttpResponse
+# from django.http import JsonResponse, HttpResponse
+from rest_framework.response import Response 
+from rest_framework.decorators import api_view
 from django.forms.models import model_to_dict
 from products.models import Product
 
+@api_view(["GET"]) # obtendo visu de API e no parenteses definindo os metodos de solicitação permitidos
 def api_home(request, *args, **kwargs):
+    """
+    DRF API VIEW
+    """
+    
+    # if request.method != "POST":
+    #     return Response({"detail": "GET not allowed"}, status=405) # tratando erro http
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     
     if model_data:
         data = model_to_dict(model_data, fields=['id', 'title', 'price'])
-    return JsonResponse(data)
-    #     print(data)
-    #     json_data_str = json.dumps(data) 
-    # return HttpResponse(json_data_str, headers={"content-type" : "application/json"}) # pegando o resultado do http e transformando em JSON
-
-    # temos que:
-    # pegar uma Instance Model e transformar em um Dicionario Python para usarmos
-    # e retornar para o client JSON
+    return Response(data)

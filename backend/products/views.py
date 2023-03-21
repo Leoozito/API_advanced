b278ed82aec934d3044d5fc01c2612268fc74f92
Leoozito <leoanrdonogueira616@gmail.com>
2023-03-21 10:18:47 -0300

    feat(Adicionais no código): :sparkles: CreateAPIView
    Adicionado metodo de visualização da API dos produtos

--- backend/products/views.py
+++ backend/products/views.py
@@ -3,6 +3,20 @@ from rest_framework import generics
 from .models import Product
 from .serializers import ProductSerializer
 
+class ProductCreateAPIView(generics.CreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def perform_create(self, serializer):
+        print(serializer.validate_data)
+        title = serializer.validate_data.get('title')
+        content = serializer.validate_data.get('content') or None
+        if content is None:
+            content = title
+        serializer.save(content=content)
+
+product_create_view = ProductCreateAPIView.as_view()
+
 # Modo de visualização da API 
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
--- py_client/detail.py
+++ py_client/detail.py
@@ -7,8 +7,8 @@ endpoint = "http://localhost:8000/api/products/1/"
 # Aqui é enviado uma response que vai direto para url
 get_response = requests.get(endpoint)
 
+print(get_response.json())
+
 # print(get_response.headers)
 # print(get_response.text)
-
-print(get_response.json())
 # print(get_response.status_code)
